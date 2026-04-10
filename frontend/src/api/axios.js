7cb99137892a4be89a8457548e43e1971a5ffe95
Ralph <bautistaralph904@gmail.com>
2026-04-10 16:23:19 +0800

    Fix: update Render URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://thefolio-api.onrender.com/api',
+  baseURL: process.env.REACT_APP_API_URL || 'https://thefolio-api-qjgz.onrender.com/api',
 });
 
 instance.interceptors.request.use((config) => {
